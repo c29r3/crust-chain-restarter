436c29de29c686d7b29b45fe9b57abd900ef8ab7
Sergey <52304443+c29r3@users.noreply.github.com>
2020-12-13 14:35:34 +0300

    Update restarter.sh

--- restarter.sh
+++ restarter.sh
@@ -3,7 +3,7 @@
 GREEN="\e[92m"
 RED="\e[91m"
 NORMAL="\e[39m"
-SLEEP_SEC=30
+SLEEP_SEC=60
 IP=$(curl -s 2ip.ru)
 TG_TOKEN=$1
 CHAT_ID=$2
@@ -27,6 +27,6 @@ do
     fi
     
     $(which docker) restart crust crust-api
-    sleep 120
+    sleep 180
   fi
 done
