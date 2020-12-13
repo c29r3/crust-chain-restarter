8a13bca5b42fe434227399580c4d7c8f15fef9f9
Sergey <52304443+c29r3@users.noreply.github.com>
2020-12-13 03:15:59 +0300

    Create restarter.sh

---
+++ restarter.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+GREEN="\e[92m"
+RED="\e[91m"
+NORMAL="\e[39m"
+SLEEP_SEC=12
+IP=$(curl -s 2ip.ru)
+TG_TOKEN=$1
+CHAT_ID=861331048
+SUBJECT="CRUST"
+
+while true
+do
+  CURRENT_BLOCK=$(curl -s 'http://localhost:56666/api/v1/block/header' | jq -r .number)
+  echo -e ${GREEN}"[$($(which date))] Current block: ${CURRENT_BLOCK}"${NORMAL}
+  sleep $SLEEP_SEC
+  NEW_BLOCK=$(curl -s 'http://localhost:56666/api/v1/block/header' | jq -r .number)
+  
+  if [[ $CURRENT_BLOCK == $NEW_BLOCK ]]
+  then
+    MSG="[$($(which date))] ${HOSTNAME} ${IP} | The block has not changed for ${SLEEP_SEC} seconds. Current block = ${CURRENT_BLOCK}"
+    echo -e ${RED}"${MSG}"${NORMAL}
+    
+    $(which curl) -s --header 'Content-Type: application/json' --request 'POST' --data "{\"chat_id\":\"${CHAT_ID}\",\"text\":\"${SUBJECT}\n${MSG}\"}" "https://api.telegram.org/bot${TOKEN}/sendMessage"
+    
+    $(which docker) restart crust crust-api
+    sleep 60
+  fi
+done
